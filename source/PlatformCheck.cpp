41e287ac43f6c2a1bb0152dc577ce399c0cf2777
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-10-12 11:00:34 +1100

    Add X11 to platform detection.

--- source/PlatformCheck.cpp
+++ source/PlatformCheck.cpp
@@ -4,9 +4,17 @@
 #ifdef Q_WS_WIN
    bool PlatformCheck::mWin = true;
    bool PlatformCheck::mMac = false;
+   bool PlatformCheck::mX11 = false;
 #endif
 
 #ifdef Q_WS_MAC
    bool PlatformCheck::mMac = true;
    bool PlatformCheck::mWin = false;
+   bool PlatformCheck::mX11 = false;
+#endif
+
+#ifdef Q_WS_X11
+   bool PlatformCheck::mMac = false;
+   bool PlatformCheck::mWin = false;
+   bool PlatformCheck::mX11 = true;
 #endif
--- source/PlatformCheck.h
+++ source/PlatformCheck.h
@@ -9,9 +9,11 @@ public:
    //~NewProjectPage();
    static bool mWin;
    static bool mMac;
+   static bool mX11;
 
    static bool isWin() { return PlatformCheck::mWin; };
    static bool isMac() { return PlatformCheck::mMac; };
+   static bool isX11() { return PlatformCheck::mX11; };
 };
 
 #endif // PLATFORMCHECK_H
